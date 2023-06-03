c67915507286d557017b5b1d019b7ceb46d81bae
Abdullah-Afzal-00 <mabdullahafzal09@gmail.com>
2023-06-03 19:39:42 +0500

    login

--- pucon-backend/routes/event.js
+++ pucon-backend/routes/event.js
@@ -3,20 +3,20 @@ const Event = require("../models/Event");
 const { verifyToken } = require("./verifyToken");
 
 router.post("/createEvent", verifyToken, (req, res) => {
-  const newEvent = new Event({
-    title: req.body.title,
-    description: req.body.description,
-    date: req.body.date,
-    time: req.body.time,
-    poster_url: req.body.poster_url,
-    userId: req.user.id,
-    duration: req.body.duration,
-  });
+	const newEvent = new Event({
+		title: req.body.title,
+		description: req.body.description,
+		date: req.body.date,
+		time: req.body.time,
+		poster_url: req.body.poster_url,
+		userId: req.user.id,
+		duration: req.body.duration,
+	});
 
-  try {
-    const savedEvent = newEvent.save();
-    res.status(200).json(savedEvent);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		const savedEvent = newEvent.save();
+		res.status(200).json(savedEvent);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
