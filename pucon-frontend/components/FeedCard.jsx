5b1e9971ce3e1efe79e5798b9c2aa3f5f21fd323
Abdullah-Afzal-00 <mabdullahafzal09@gmail.com>
2023-06-03 15:27:12 +0500

    searchbar added

--- pucon-frontend/components/FeedCard.jsx
+++ pucon-frontend/components/FeedCard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const FeedCard = () => {
-	return <div>FeedCard</div>;
+	return (
+		<>
+			<div className="rounded-lg w-auto h-auto bg-slate-300">
+				<div className="rounded-lg w-80">Name</div>
+				<div className="rounded-lg w-80">Detail</div>
+				<img className="rounded-lg object-cover h-48 w-80" src="https://picsum.photos/200/300" />
+			</div>
+		</>
+	);
 };
 
 export default FeedCard;
