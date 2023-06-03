20cb55882989c727264006431f3c390b5875983f
ahsan722505 <ahsantahir722505@gmail.com>
2023-06-03 12:11:14 +0500

    backend

--- pucon-backend/models/User.js
+++ pucon-backend/models/User.js
@@ -5,10 +5,6 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    user_type: { type: String, required: true },
-    gender: { type: String },
-    DOB: { type: String },
-    img: { type: String },
   },
   { timestamps: true }
 );
