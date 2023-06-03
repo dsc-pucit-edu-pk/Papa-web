0927d06fb470540889d72485bc0903847339cd53
Abdullah-Afzal-00 <mabdullahafzal09@gmail.com>
2023-06-03 16:43:54 +0500

    FeedCard

--- pucon-frontend/components/FeedCard.jsx
+++ pucon-frontend/components/FeedCard.jsx
@@ -3,10 +3,12 @@ import React from "react";
 const FeedCard = () => {
 	return (
 		<>
-			<div className="rounded-lg w-auto h-auto bg-slate-300">
-				<div className="rounded-lg w-80">Name</div>
-				<div className="rounded-lg w-80">Detail</div>
-				<img className="rounded-lg object-cover h-48 w-80" src="https://picsum.photos/200/300" />
+			<div className="flex items-center justify-center">
+				<div className="rounded-lg w-[60%] h-auto bg-blue-100">
+					<div className="rounded-lg w-[60%]">Name</div>
+					<div className="rounded-lg w-[60%]">Detail</div>
+					<img className="rounded-lg object-cover h-80 w-[100%]" src="https://picsum.photos/200/300" />
+				</div>
 			</div>
 		</>
 	);
