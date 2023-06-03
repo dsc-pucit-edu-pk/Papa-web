7ae4b7292ec362a219c621bd919464730d8c32bc
abubakarafzal444 <abubakarafzal444@gmail.com>
2023-06-03 13:44:21 +0500

    side layout added

--- pucon-frontend/components/Layout.tsx
+++ pucon-frontend/components/Layout.tsx
@@ -2,22 +2,22 @@ import * as React from "react";
 import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import CssBaseline from "@mui/material/CssBaseline";
 import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import FeedOutlinedIcon from "@mui/icons-material/FeedOutlined";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 const drawerWidth = 240;
 
@@ -30,13 +30,13 @@ const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  marginRight: -drawerWidth,
+  marginLeft: `-${drawerWidth}px`,
   ...(open && {
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
-    marginRight: 0,
+    marginLeft: 0,
   }),
 }));
 
@@ -53,11 +53,11 @@ const AppBar = styled(MuiAppBar, {
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
     transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
-    marginRight: drawerWidth,
   }),
 }));
 
@@ -67,12 +67,14 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-  justifyContent: "flex-start",
+  justifyContent: "flex-end",
 }));
 
-export default function PersistentDrawerRight() {
-  const theme = useTheme();
+export default function Layout({ children }) {
   const [open, setOpen] = React.useState(false);
+  const router = useRouter();
+  const pathname = router.pathname;
+  console.log(pathname);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -85,102 +87,97 @@ export default function PersistentDrawerRight() {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar position="fixed" open={open}>
+      <AppBar position="fixed" color="transparent" open={open}>
         <Toolbar>
-          <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
-            Persistent drawer
-          </Typography>
           <IconButton
             color="inherit"
             aria-label="open drawer"
-            edge="end"
             onClick={handleDrawerOpen}
-            sx={{ ...(open && { display: "none" }) }}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
           >
             <MenuIcon />
           </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Persistent drawer
+          </Typography>
         </Toolbar>
       </AppBar>
-      <Main open={open}>
-        <DrawerHeader />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Main>
       <Drawer
         sx={{
           width: drawerWidth,
           flexShrink: 0,
           "& .MuiDrawer-paper": {
             width: drawerWidth,
+            boxSizing: "border-box",
+            backgroundColor: "#101827",
+            color: "white",
+            paddingX: "10px",
+            paddingTop: "5px",
           },
         }}
         variant="persistent"
-        anchor="right"
+        anchor="left"
         open={open}
       >
-        <DrawerHeader>
+        <DrawerHeader sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Image src="/icons/logo.svg" alt="logo" width="32" height="32" />
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
+            <ChevronLeftIcon sx={{ color: "white" }} />
           </IconButton>
         </DrawerHeader>
         <Divider />
+        <div className="ml-2 mt-3">
+          <p
+            className="text-[#818cf8] uppercase font-bold text-xs"
+            style={{ letterSpacing: "1px" }}
+          >
+            Dashboard
+          </p>
+          <p className="text-[11px] text-[#9ca3af] font-semibold mt-0.5">
+            Explore tools to make life easier
+          </p>
+        </div>
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          <Link href="/feed">
+            <ListItem key={"Feed"} disablePadding>
+              <ListItemButton
+                sx={{
+                  background:
+                    pathname == "/feed" &&
+                    "rgba(255, 255, 255, 0.1) !important",
+                }}
+              >
+                <ListItemIcon sx={{ minWidth: "unset" }}>
+                  {<FeedOutlinedIcon sx={{ color: "white" }} />}
                 </ListItemIcon>
-                <ListItemText primary={text} />
+                <p className="text-sm font-medium ml-4">Feed</p>
               </ListItemButton>
             </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          </Link>
+          <Link href="/calendar">
+            <ListItem key={"Calendar"} disablePadding>
+              <ListItemButton
+                sx={{
+                  background:
+                    pathname == "/calendar" &&
+                    "rgba(255, 255, 255, 0.1) !important",
+                }}
+              >
+                <ListItemIcon sx={{ minWidth: "unset" }}>
+                  {<FeedOutlinedIcon sx={{ color: "white" }} />}
                 </ListItemIcon>
-                <ListItemText primary={text} />
+
+                <p className="text-sm font-medium ml-4">Calendar</p>
               </ListItemButton>
             </ListItem>
-          ))}
+          </Link>
         </List>
       </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        {children}
+      </Main>
     </Box>
   );
 }
